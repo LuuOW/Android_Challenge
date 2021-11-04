76a1c2b3fe970f8b4c78bc4e537b089145b491bd
GerardoRML <gerardo.ramirez@mercadolibre.com>
2021-11-04 11:39:23 -0300

    Adding recyclerView in activity_list_type.xml

--- app/src/main/java/com/lucaskempe/android_challenge/view/TermActivity.kt
+++ app/src/main/java/com/lucaskempe/android_challenge/view/TermActivity.kt
@@ -18,6 +18,7 @@ class TermActivity : AppCompatActivity() {
         binding.btnClose.setOnClickListener {
             intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
